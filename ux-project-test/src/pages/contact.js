17dd2c3fd5f9e4504853c37ebffd8a41cc92a04e
Rebecca Nettleton <nettr2@student.op.ac.nz>
2020-09-25 17:36:56 +1200

    test

--- ux-project-test/src/pages/contact.js
+++ ux-project-test/src/pages/contact.js
@@ -6,28 +6,25 @@ import Navigation from "../components/navigation"
 export default function Contact() {
   return (
     <div>
-      {" "}
       <Navigation></Navigation>
       <Layout>
         <Header headerText="Contact Page"></Header>
         <form name="contact" method="POST" data-netlify="true">
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address: </label>
+          <p class="form-group">
+            <label >Email address: </label>
             <input
               type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+              class="form-control"   
             ></input>
-          </div>
-          <div class="form-group">
-            <label for="inputphone">Phone: </label>
-            <input type="text" class="form-control" id="inputphone"></input>
-          </div>
-          <div class="form-group">
-            <label for="inputAddress">Address: </label>
-            <input type="text" class="form-control" id="inputAddress"></input>
-          </div>
+          </p>
+          <p class="form-group">
+            <label >Phone: </label>
+            <input type="text" class="form-control"></input>
+          </p>
+          <p class="form-group">
+            <label>Address: </label>
+            <input type="text" class="form-control" ></input>
+          </p>
           <button type="submit" class="btn btn-primary">
             Submit
           </button>
